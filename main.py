e8cb558b95e013d2edb5b8b7ecdf72c24bd1dcf9
SkyJIioN <lnekrol@gmail.com>
2025-06-05 13:51:59 +0300

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import requests
 from telegram import Update, Bot
 from telegram.ext import Application, CommandHandler, ContextTypes
 from groq import Groq
-from fastapi import FastAPI, 
-Request
+from fastapi import FastAPI, Request
 import uvicorn
 
 # Логування
